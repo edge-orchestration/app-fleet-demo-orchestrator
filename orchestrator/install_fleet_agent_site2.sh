2e02972be66ad112cdace1e5b09c0c49726cbc54
Bassem Debbabi <bassem.debbabi@atos.net>
2023-03-07 20:25:02 +0100

    updating agent site2 install script

--- orchestrator/install_fleet_agent_site2.sh
+++ orchestrator/install_fleet_agent_site2.sh
@@ -4,3 +4,19 @@ helm --kube-context microk8s -n cattle-fleet-system install --create-namespace -
     ${CLUSTER2_LABELS} \
     --values values.yaml \
     --debug fleet-agent https://github.com/rancher/fleet/releases/download/v0.6.0-rc.4/fleet-agent-0.6.0-rc.4.tgz
+
+export ARCH=$(case $(uname -m) in x86_64) echo -n amd64 ;; aarch64) echo -n arm64 ;; *) echo -n $(uname -m) ;; esac)
+export OS=$(uname | awk '{print tolower($0)}')    
+
+export OPERATOR_SDK_DL_URL=https://github.com/operator-framework/operator-sdk/releases/download/v1.27.0
+curl -LO ${OPERATOR_SDK_DL_URL}/operator-sdk_${OS}_${ARCH}
+
+gpg --keyserver keyserver.ubuntu.com --recv-keys 052996E2A20B5C7E
+curl -LO ${OPERATOR_SDK_DL_URL}/checksums.txt
+curl -LO ${OPERATOR_SDK_DL_URL}/checksums.txt.asc
+gpg -u "Operator SDK (release) <cncf-operator-sdk@cncf.io>" --verify checksums.txt.asc
+grep operator-sdk_${OS}_${ARCH} checksums.txt | sha256sum -c -
+
+chmod +x operator-sdk_${OS}_${ARCH} && sudo mv operator-sdk_${OS}_${ARCH} /usr/local/bin/operator-sdk
+
+operator-sdk olm install 
